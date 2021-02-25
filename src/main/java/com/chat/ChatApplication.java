e9fe3dd5a73984e274817f60ed866d8487b22cb6
Joydeep <joydeep.bhattacharjee@telekom.de>
2021-02-25 16:23:01 +0100

    update

--- src/main/java/com/chat/ChatApplication.java
+++ src/main/java/com/chat/ChatApplication.java
@@ -2,21 +2,18 @@ package com.chat;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ChatApplication {
 
 	private static BlockingQueue<String> initiatorSends = new ArrayBlockingQueue<String>(1);
 	private static BlockingQueue<String> initiatorRecives = new ArrayBlockingQueue<String>(1);
-	static AtomicInteger counter = new AtomicInteger();
-
+	static int counter;
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Start................");
 		Thread thread1 = new Thread(() -> initiatorPlayer());
 		Thread thread2 = new Thread(() -> otherPlayer());
 		thread1.start();
 		thread2.start();
-
 		thread1.join();
 		thread2.join();
 		System.out.println("End.........");
@@ -25,36 +22,36 @@ public class ChatApplication {
 	private static void initiatorPlayer() {
 
 		try {
-			String startMsg = String.format("Start  {%d} ", counter.get());
-			System.out.println("Initial Player : "+startMsg);
+			String startMsg = String.format("Start  {%d} ", counter);
+			System.out.println("Initial Player : " + startMsg);
 			initiatorSends.put(startMsg);
-			while (counter.get() < 19) {
+			while (counter < 9) {
 				String msg = initiatorRecives.take();
-				// System.out.println("Initiator player msg "+msg);
-				msg = msg + String.format(" {%d} ", counter.incrementAndGet());
-				System.out.println("Initial Player : "+msg);
+				msg = msg + String.format(" {%d} ", ++counter);
+				System.out.println("Initial Player : " + msg);
 				initiatorSends.put(msg);
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			handleError(e);
 		}
 	}
 
 	private static void otherPlayer() {
 		String msg;
 		try {
-			while (counter.get() < 19) {
+			while (counter < 9) {
 				msg = initiatorSends.take();
-				//System.out.println("Player1  msg recive " + msg);
-				msg = msg + String.format(" {%d} ", counter.incrementAndGet());
-				System.out.println("Second Player : "+msg);
+				System.out.println("Second  Player : " + msg);
 				initiatorRecives.put(msg);
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			handleError(e);
 		}
+	}
+
+	private static void handleError(Exception e) {
+		System.err.println("Thread interrupted unexpectedly ");
+		throw new IllegalStateException("Program ended unexpectedly");
 	}
 
 }
